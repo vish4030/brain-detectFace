6aae2b9031bd16f022aa26a30035c7fb47326604
amit kumar <amitkumar@amits-MacBook-Air.local>
2021-06-09 02:43:19 +0530

    adding

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/register', {
+    fetch('https://fathomless-inlet-74348.herokuapp.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
